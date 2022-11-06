e8709d6fab319418bedf259d66c9296a90a5ba19
XeonE52680v3 <minh15052008@gmail.com>
2022-11-06 20:00:32 +0700

    Upload source code

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Client, Message, GatewayIntentBits } from "discord.js";
+import { prefix, token } from "./config.json";
+
+const client: Client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.MessageContent,
+  ]
+});
+
+client.once("ready", () => {
+  console.log("Bot is ready!");
+});
+
+client.on("messageCreate", async (message: Message) => {
+  if (message.content.startsWith(`${prefix}ping`)) {
+    message.channel.send("🚀 pong");
+    // message.reply('pong!');
+  }
+});
+
+client.login(token);
